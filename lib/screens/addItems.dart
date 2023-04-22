d8a2ed420cbf9f1485bee93b0412162a25af1e04
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-04-22 15:52:46 +0700

    🎨 create class location

---
+++ lib/screens/addItems.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AddNewItems extends StatefulWidget {
+  const AddNewItems({super.key});
+
+  @override
+  State<AddNewItems> createState() => _AddNewItemsState();
+}
+
+class _AddNewItemsState extends State<AddNewItems> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
